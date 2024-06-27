387672010abaa6043ccbc327454844166785bb9c
christianCode <christiancode@alwaysdata.net>
2024-06-27 11:14:30 +0100

    mise à jour 3 des services

--- ecom_ui/ecom/views.py
+++ ecom_ui/ecom/views.py
@@ -14,6 +14,14 @@ def register_view(request):
 
 
 
+def get_session_data(request):
+    if 'user' in request.session:
+        user_data = request.session['user']
+        return JsonResponse(user_data)
+    else:
+        return JsonResponse({'error': 'Session data not found'}, status=404)
+
+
 def home_view(request):
 
     access_token = request.COOKIES.get('access_token')
@@ -24,20 +32,20 @@ def home_view(request):
         'Authorization': f'Bearer {access_token}'
     }
 
-    user = requests.get('http://127.0.0.1:8000/api/customer', headers=headers)
+    user = get_session_data()
+
     products = requests.get('http://127.0.0.1:8000/api/products', headers=headers)
 
-    if user.status_code == 200 and products.status_code == 200:
-        user_data = user.json()
+    if products.status_code == 200:
         products_datas = products.json()
-        return render(request, 'index.html', {'user': user_data, 'products': products_datas}, safe=False)
+        return render(request, 'index.html', {'user': user, 'products': products_datas}, safe=False)
     else:
         return redirect('login')
 
 
 
 def card_view(request):
-    
+
     access_token = request.COOKIES.get('acces_token')
     if not access_token:
         return redirect('login')
@@ -45,7 +53,8 @@ def card_view(request):
         headers = {
             'Authorization': f'Bearer {access_token}'
         }
-        cards = requests.get('http://127.0.0.1:8000/api/cards', headers=headers)
+        user = get_session_data()
+        cards = requests.get(f'http://127.0.0.1:8000/api/cards/{user.id}', headers=headers)
     
         if cards.status_code == 200:
             cards_datas = cards.json()
@@ -60,17 +69,16 @@ def profil_view(request):
     access_token = request.COOKIES.get('access_token')
     if not access_token:
         return redirect('login')
-
-    headers = {
-        'Authorization': f'Bearer {access_token}'
-    }
-
-    response = requests.get('http://127.0.0.1:8000/api/customer', headers=headers)
-    if response.status_code == 200:
-        user_data = response.json()
-        return render(request, 'profil.html', {'user': user_data})
     else:
-        return redirect('login')
+        headers = {
+            'Authorization': f'Bearer {access_token}'
+        }
+        if response.status_code == 200:
+            user = get_session_data()
+            response = requests.get(f'http://127.0.0.1:8000/api/customer/{user.id}', headers=headers)
+            return render(request, 'profil.html', {'user': user})
+        else:
+            return redirect('login')
 
 
 
@@ -83,7 +91,9 @@ def notification_view(request):
         headers = {
             'Authorization': f'Bearer {access_token}'
         }
-        notifications = requests.get('http://127.0.0.1:8000/api/notifications', headers=headers)
+
+        user = get_session_data()
+        notifications = requests.get(f'http://127.0.0.1:8000/api/notifications/{user.id}', headers=headers)
     
         if notifications.status_code == 200:
             notifications_datas = notifications.json()
