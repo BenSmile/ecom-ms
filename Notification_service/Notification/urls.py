bbe761178655ba4fa88578399a83a26e41015fa7
christianCode <christiancode@alwaysdata.net>
2024-06-27 11:11:39 +0100

    mise à jour 2 des services

--- Notification_service/Notification/urls.py
+++ Notification_service/Notification/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 from .views import notification_list
 
 urlpatterns = [
-    path('notifications/', notification_list, name='notification_list'),
+    path('notifications/<int:user_id>', notification_list, name='notification_list'),
 ]
--- Notification_service/Notification/views.py
+++ Notification_service/Notification/views.py
@@ -3,6 +3,6 @@
 from django.shortcuts import render
 from .models import Notification
 
-def notification_list(request):
-    notifications = Notification.objects.all()
+def notification_list(request, user_id):
+    notifications = Notification.objects.filter(user_id=user_id)
     return render(request, 'notification_list.html', {'notifications': notifications})
