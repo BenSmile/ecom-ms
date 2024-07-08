fce4637211bb77550b450075843aaabab3087cec
christianCode <christiancode@alwaysdata.net>
2024-07-08 14:43:23 +0100

    unified files

--- Notification_service/Notification/urls.py
+++ Notification_service/Notification/urls.py
@@ -2,7 +2,9 @@
 
 from django.urls import path
 from .views import notification_list
+import notification_consumer
 
 urlpatterns = [
-    path('notifications/<int:user_id>', notification_list, name='notification_list'),
+    path('/<int:user_id>', notification_list, name='notification_list'),
+    path('/create/<int:user_id>', notification_consumer.callback, name='create_notification'),
 ]
